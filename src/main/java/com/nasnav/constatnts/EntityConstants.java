41a2afe7f256446b1adb63bf52bd48a82122a025
admin <admin@WS>
2019-04-24 09:05:42 +0200

    MOD : move Roles enum to separate class

--- src/main/java/com/nasnav/constatnts/EntityConstants.java
+++ src/main/java/com/nasnav/constatnts/EntityConstants.java
@@ -47,26 +47,4 @@ public final class EntityConstants {
         }
     }
 
-    /*
-    public enum Roles {
-
-        CUSTOMER("CUSTOMER"),
-        ORGANIZATION_MANAGER("ORGANIZATION_MANAGER"),
-        STORE_ADMIN("STORE_ADMIN"),
-        STORE_MANAGER("STORE_MANAGER"), 
-        NASNAV_ADMIN("NASNAV_ADMIN"),
-        ORGANIZATION_ADMIN("ORGANIZATION_ADMIN"),	
-    	ORGANIZATION_EMPLOYEE("ORGANIZATION_EMPLOYEE"),
-    	STORE_EMPLOYEE("STORE_EMPLOYEE");
-
-        @Getter
-        private final String value;
-
-        Roles(String value) {
-            this.value = value;
-        }
-
-    }
-    */
-
 }
