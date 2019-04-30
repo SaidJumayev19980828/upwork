012e3bfe35dff1717d499d64fee21e1eabcb6de4
Riham Nour <riham.nour@nasnav.com>
2019-04-30 15:58:58 +0200

    MOD : adjust session initialize API response object

--- src/main/java/com/nasnav/dto/OrderSessionResponse.java
+++ src/main/java/com/nasnav/dto/OrderSessionResponse.java
@@ -1,27 +1,41 @@
 package com.nasnav.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.nasnav.persistence.BasketsEntity;
 import lombok.Data;
 import lombok.Getter;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.List;
 
 @Data
-public class OrderSessionResponse {
+public class OrderSessionResponse implements Serializable {
 
     private boolean success;
 
-    private String session_id;
+    @JsonProperty("session")
+    private ResponseSession session;
 
+    @JsonProperty("merchant")
     private String merchant_id;
 
+    @JsonProperty("id")
     private String order_ref;
 
+    @JsonProperty("basket")
     private List<OrderSessionBasket> basket;
 
+    @JsonProperty("order_value")
     private BigDecimal order_value;
 
+    @JsonProperty("currency")
     private Currency order_currency;
+
+    @Data
+    public static class ResponseSession implements Serializable{
+        @JsonProperty("id")
+        private String id;
+    }
 
 }
