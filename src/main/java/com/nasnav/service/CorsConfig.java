beced57bebd2b7eaff214223e4c49ea7ce3395cb
marek <marek@zachara.name>
2019-10-08 23:18:22 +0200

    CORS adjustments

--- src/main/java/com/nasnav/service/CorsConfig.java
+++ src/main/java/com/nasnav/service/CorsConfig.java
@@ -5,12 +5,15 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
+@Configuration
 //@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*");
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins("*")
+                .allowedHeaders("*");
     }
 }
