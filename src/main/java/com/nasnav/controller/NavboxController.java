93cee7942a09c53557b4884b63c4fe7a8d8c7d64
ahmed.galal <ahmed.galal@nasnav.com>
2020-01-21 15:06:44 +0200

    still trying to make GET /navbox/categories return empty array

--- src/main/java/com/nasnav/controller/NavboxController.java
+++ src/main/java/com/nasnav/controller/NavboxController.java
@@ -144,11 +144,9 @@ public class NavboxController {
 			@io.swagger.annotations.ApiResponse(code = 406, message = "invalid search parameter")
 	})
 	@GetMapping(value="/categories",produces=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> getCategories(@RequestParam(name = "org_id", required = false) Long organizationId,
+	public List<CategoryRepresentationObject> getCategories(@RequestParam(name = "org_id", required = false) Long organizationId,
 										   @RequestParam(name = "category_id", required = false) Long categoryId) throws BusinessException {
-		List<CategoryRepresentationObject> response = categoryService.getCategories(organizationId, categoryId);
-
-		return response.isEmpty() ? new ResponseEntity<>(response,HttpStatus.NO_CONTENT): new ResponseEntity<>(response, HttpStatus.OK);
+		return categoryService.getCategories(organizationId, categoryId);
 	}
 
 
