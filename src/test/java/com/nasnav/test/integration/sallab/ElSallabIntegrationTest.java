c7726705c51acd4d1babbb689833a325647a6794
ahmed.galal <ahmed.galal@nasnav.com>
2020-02-05 16:53:20 +0200

    working on product integration tests for el-sallab

---
+++ src/test/java/com/nasnav/test/integration/sallab/ElSallabIntegrationTest.java
@@ -0,0 +1,222 @@
+package com.nasnav.test.integration.sallab;
+
+import static com.nasnav.test.commons.TestCommons.getHttpEntity;
+import static com.nasnav.test.commons.TestCommons.json;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockserver.model.HttpRequest.request;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockserver.junit.MockServerRule;
+import org.mockserver.verify.VerificationTimes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.nasnav.NavBox;
+import com.nasnav.dao.BrandsRepository;
+import com.nasnav.dao.IntegrationMappingRepository;
+import com.nasnav.dao.ProductRepository;
+import com.nasnav.dao.ProductVariantsRepository;
+import com.nasnav.dao.ShopsRepository;
+import com.nasnav.dto.OrganizationIntegrationInfoDTO;
+import com.nasnav.exceptions.BusinessException;
+import com.nasnav.integration.IntegrationService;
+import com.nasnav.persistence.ProductVariantsEntity;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = NavBox.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@PropertySource("classpath:database.properties")
+@AutoConfigureWebTestClient
+@Sql(executionPhase=ExecutionPhase.BEFORE_TEST_METHOD,  scripts={"/sql/El_sallab_integration_Test_Data_Insert.sql"})
+@Sql(executionPhase=ExecutionPhase.AFTER_TEST_METHOD, scripts={"/sql/database_cleanup.sql"})
+@DirtiesContext
+public class ElSallabIntegrationTest {
+	
+	private static final String SALLAB_SERVER_URL = "https://azizsallab--DevSanbox.cs80.my.salesforce.com";    
+    private static final String SALLAB_SEVER_URL_2 = "http://41.33.113.70";
+    private static final String AUTH_SERVER_URL = "https://test.salesforce.com";
+    private static final String MOCK_SERVER_URL = "http://127.0.0.1";
+    
+  private static final String SERVER_URL = MOCK_SERVER_URL;
+//  private static final String SERVER_URL = SALLAB_SERVER_URL;
+  
+    private static final boolean usingMockServer = SERVER_URL.equals(MOCK_SERVER_URL);
+	
+    
+	private static final Long ORG_ID = 99001L;
+	
+	
+	@Autowired
+	private IntegrationService integrationService;
+	
+	
+	@Autowired
+	private ElSallabIntegrationTestCommon testCommons;
+	
+	@Autowired
+	private ProductRepository productRepo;
+	
+	@Autowired
+	private ShopsRepository shopsRepo;
+	
+	@Autowired
+    private TestRestTemplate template;
+	
+	@Autowired
+	private BrandsRepository brandRepo;
+	
+	@Autowired
+	private ProductVariantsRepository variantRepo;
+	
+	@Autowired
+	private IntegrationMappingRepository mappingRepo;
+	
+	@Rule
+	 public MockServerRule mockServerRule = new MockServerRule(this);
+	
+	
+	
+	
+	@Before
+	public void init() throws Exception {			
+		String serverFullUrl = SALLAB_SERVER_URL;
+		String server2FullUrl = SALLAB_SEVER_URL_2;
+		String authServerUrl = AUTH_SERVER_URL;
+		
+		if(usingMockServer) {
+			serverFullUrl = testCommons.initElSallabMockServer(mockServerRule);
+			server2FullUrl = serverFullUrl;
+			authServerUrl = serverFullUrl;
+		}
+		
+		registerIntegrationModule(serverFullUrl, server2FullUrl, authServerUrl);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	@Test
+	@Sql(executionPhase=ExecutionPhase.BEFORE_TEST_METHOD,  scripts={"/sql/MS_dynamics_integration_products_import_test_data.sql"})
+	@Sql(executionPhase=ExecutionPhase.AFTER_TEST_METHOD, scripts={"/sql/database_cleanup.sql"})
+	public void importProductsTest() throws Throwable {
+		int count = 7;
+		long countProductsBefore = productRepo.count();
+		long countShopsBefore = shopsRepo.count();
+		
+		//------------------------------------------------		
+		//call product import api
+		JSONObject requestJson = 
+				json()
+					.put("dryrun", false)
+					.put("update_product", true)
+					.put("update_stocks", true)
+					.put("currency", 1)
+					.put("encoding", "UTF-8")
+					.put("page_count", count);
+		
+		HttpEntity<Object> request = getHttpEntity(requestJson.toString(), "hijkllm");
+        ResponseEntity<Integer> response = template.exchange("/integration/import/products", HttpMethod.POST, request, Integer.class);       
+        		
+		//------------------------------------------------
+		//test the mock api was called
+		if(usingMockServer) {
+			mockServerRule.getClient().verify(
+				      request()
+				        .withMethod("GET")
+				        .withPath("/api/products/\\d+/\\d+"),
+				      VerificationTimes.exactly(1)
+				    );
+		}
+		//------------------------------------------------
+		//test imported brands were created
+		//test the imported products were created
+		
+		long countProductsAfter = productRepo.count();
+		long countShopsAfter = shopsRepo.count();
+		JSONArray extShopsJson = getExpectedShopsJson();
+		
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertNotEquals("products were imported", 0L, countProductsAfter - countProductsBefore);
+		if(usingMockServer) {
+			assertEquals("shops were imported", extShopsJson.length() - countShopsBefore, countShopsAfter - countShopsBefore);
+			assertEquals("assert brands were imported", 3L, brandRepo.count());
+			assertTrue("all imported products have integration mapping" , allProductHaveMapping());
+			assertEquals("check number of remaining pages to import", 0, response.getBody().intValue());
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	private void registerIntegrationModule(String serverFullUrl, String server2FullUrl, String authServerUrl) throws BusinessException {
+		Map<String,String> params = new HashMap<>();
+		params.put("SERVER_URL", serverFullUrl);
+		params.put("SERVER2_URL", server2FullUrl);
+		params.put("AUTH_SERVER_URL", authServerUrl);
+		
+		OrganizationIntegrationInfoDTO integrationInfo = new OrganizationIntegrationInfoDTO();
+		integrationInfo.setIntegrationModule("com.nasnav.integration.elsallb.ElSallabIntegrationModule");
+		integrationInfo.setMaxRequestRate(1);
+		integrationInfo.setOrganizationId(99001L);
+		integrationInfo.setIntegrationParameters(params);
+		
+		integrationService.registerIntegrationModule(integrationInfo);
+	}
+	
+	
+	
+
+	private boolean allProductHaveMapping() {
+		return variantRepo
+				.findByOrganizationId(ORG_ID)
+				.stream()
+				.map(ProductVariantsEntity::getId)
+				.map(id -> id.toString())
+				.map(id -> mappingRepo.findByOrganizationIdAndMappingType_typeNameAndLocalValue(ORG_ID, "PRODUCT_VARIANT", id))
+				.allMatch(Optional::isPresent);			
+	}
+	
+	
+	
+	private JSONArray getExpectedShopsJson() throws IOException {
+		JSONArray extShopsJson = new JSONArray();
+		return extShopsJson;
+	}
+	
+
+}
