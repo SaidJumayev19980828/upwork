146cd897d371c2366e07b9eb440668291d727432
ahmed.galal <ahmed.galal@nasnav.com>
2020-02-24 17:00:13 +0200

    working on the integration event for importing images

---
+++ src/main/java/com/nasnav/integration/events/data/ImageImportParam.java
@@ -0,0 +1,19 @@
+package com.nasnav.integration.events.data;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper=false)
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class ImageImportParam{
+	private Integer type;
+	private Integer priority;
+}
