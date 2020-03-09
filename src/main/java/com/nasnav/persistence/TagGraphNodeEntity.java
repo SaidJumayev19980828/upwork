8ab26d3c482a8e4143086432e4183db42a3fcc9b
ahmed.galal <ahmed.galal@nasnav.com>
2020-03-09 16:58:59 +0200

    add graph node entity

---
+++ src/main/java/com/nasnav/persistence/TagGraphNodeEntity.java
@@ -0,0 +1,25 @@
+package com.nasnav.persistence;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Table(name = "tag_graph_nodes")
+@Entity
+@Data
+public class TagGraphNodeEntity {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	
+	@ManyToOne
+	@JoinColumn(name = "tag_id", nullable = false)
+	private TagsEntity tag;
+}
