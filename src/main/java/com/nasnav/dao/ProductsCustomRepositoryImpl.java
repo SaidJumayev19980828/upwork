1656805c8ce71ab8bca36246d3469df07dbe2c15
Ahmad Abdel-Salam <ahmad.18750@yahoo.com>
2020-06-03 22:09:50 +0000

    fix bug in filtering the products by tags in GET /navbox/filters this fixes #7

--- src/main/java/com/nasnav/dao/ProductsCustomRepositoryImpl.java
+++ src/main/java/com/nasnav/dao/ProductsCustomRepositoryImpl.java
@@ -2,6 +2,13 @@ package com.nasnav.dao;
 
 import java.util.Set;
 
+import com.nasnav.model.querydsl.sql.QProductVariants;
+import com.nasnav.model.querydsl.sql.QProducts;
+import com.nasnav.model.querydsl.sql.QStocks;
+import com.nasnav.request.ProductSearchParam;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.sql.SQLQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +17,8 @@ import com.nasnav.model.querydsl.sql.QProductTags;
 import com.nasnav.service.model.ProductTagPair;
 import com.querydsl.sql.SQLQueryFactory;
 import com.querydsl.sql.dml.SQLInsertClause;
+
+import static com.querydsl.sql.SQLExpressions.select;
 
 @Repository
 @Transactional
@@ -31,7 +40,43 @@ public class ProductsCustomRepositoryImpl implements ProductsCustomRepository {
 		}
 	}
 	
-	
+	@Override
+	public SQLQuery<?> getProductsBaseQuery(BooleanBuilder predicate, ProductSearchParam params) {
+		QStocks stock = QStocks.stocks;
+		QProducts product = QProducts.products;
+		QProductVariants variant = QProductVariants.productVariants;
+		QProductTags productTags = QProductTags.productTags;;
+
+		SQLQuery<?> baseQuery = queryFactory.from(stock)
+				.innerJoin(variant).on(stock.variantId.eq(variant.id))
+				.innerJoin(product).on(variant.productId.eq(product.id))
+				.where(predicate);
+
+		SQLQuery<?> productTagsQuery = getProductTagsQuery(queryFactory, productTags, params);
+
+		if (productTagsQuery != null)
+			baseQuery.where(product.id.in((com.querydsl.core.types.Expression<? extends Long>) productTagsQuery));
+
+		return baseQuery;
+	}
+
+
+	private SQLQuery<Long> getProductTagsQuery(SQLQueryFactory query, QProductTags productTags, ProductSearchParam params) {
+		if (params.getTags() == null)
+			return null;
+
+		return query.select(Expressions.numberPath(Long.class, "id"))
+				.from(
+						select(
+								productTags.productId.as("id")
+								, productTags.tagId.count().as("count"))
+								.from(productTags)
+								.where(productTags.tagId.in(params.getTags()))
+								.groupBy(productTags.productId)
+								.having(productTags.tagId.count().eq((long) params.getTags().size()))
+								.as("productTags"));
+	}
+
 	
 	private void  insertProductTag(QProductTags productTags, SQLInsertClause insert,
 			ProductTagPair productTag) {
