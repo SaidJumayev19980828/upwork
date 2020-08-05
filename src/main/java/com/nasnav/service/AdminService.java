b4d07e18d3962b444fc1ce8cd9457fc8609e8e18
Ahmad.Abdel-Salam <ahmad.abdel-salam@nasnav.com>
2020-08-05 22:24:38 +0200

    make the cache invalidate newly added caches automatically

--- src/main/java/com/nasnav/service/AdminService.java
+++ src/main/java/com/nasnav/service/AdminService.java
@@ -1,14 +1,19 @@
 package com.nasnav.service;
 
-import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Service;
 
-import static com.nasnav.cache.Caches.*;
 
 @Service
 public class AdminService {
 
-    @CacheEvict(cacheNames = {SHOPS_BY_ID, ORGANIZATIONS_TAGS, ORGANIZATIONS_TAG_TREES, ORGANIZATIONS_CATEGORIES, ORGANIZATIONS_EXTRA_ATTRIBUTES,
-                              ORGANIZATIONS_SHOPS, ORGANIZATIONS_DOMAINS, ORGANIZATIONS_BY_ID, BRANDS, FILES, USERS_BY_TOKENS})
-    public void invalidateCaches() {}
+    @Autowired
+    private CacheManager cacheManager;
+
+    public void invalidateCaches() {
+        cacheManager.getCacheNames()
+                    .stream()
+                    .forEach(cacheName -> cacheManager.getCache(cacheName).clear());
+    }
 }
