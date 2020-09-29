a05a69cc1f4de63741d3873b3177a156a79d0228
Ahmad.Abdel-Salam <ahmad.abdel-salam@nasnav.com>
2020-09-29 11:27:08 +0200

    remove shops images from GET /navbox/organization hide warehouses from GET /navbox/shops

--- src/main/java/com/nasnav/dao/OrganizationImagesRepository.java
+++ src/main/java/com/nasnav/dao/OrganizationImagesRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface OrganizationImagesRepository extends CrudRepository<OrganizationImagesEntity, Long> {
 
-    List<OrganizationImagesEntity> findByOrganizationEntityIdAndTypeNotIn(Long id, List<Integer> types);
+    List<OrganizationImagesEntity> findByOrganizationEntityIdAndShopsEntityNullAndTypeNotIn(Long id, List<Integer> types);
     List<OrganizationImagesEntity> findByShopsEntityIdAndTypeNot(Long id, Integer type);
 
     List<OrganizationImagesEntity> findByOrganizationEntityIdAndTypeOrderByIdDesc(Long id, Integer type);
