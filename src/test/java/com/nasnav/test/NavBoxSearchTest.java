2f59c0d6e4d474afc6ceccc574498eece73e924c
ahmed.galal <ahmed.galal@nasnav.com>
2020-12-07 00:00:49 +0200

    preparing tests and configurations
    - disabled the default elastic search auto configuration, as it is not compatible with the newest version 7.10.0
    - upgraded the elastic search dependencies to 7.10.0
    - set a test with a mock server the imitates expected elastic search api response.

--- src/test/java/com/nasnav/test/NavBoxSearchTest.java
+++ src/test/java/com/nasnav/test/NavBoxSearchTest.java
@@ -1,25 +1,36 @@
 package com.nasnav.test;
 
+import com.google.common.net.UrlEscapers;
 import com.nasnav.NavBox;
 import net.jcip.annotations.NotThreadSafe;
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockserver.junit.MockServerRule;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.util.UriBuilder;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.net.URI;
 
 import static com.google.common.net.MediaType.JSON_UTF_8;
 import static com.nasnav.test.commons.TestCommons.readResourceFileAsString;
+import static org.junit.Assert.assertEquals;
 import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
+import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
 
@@ -31,24 +42,54 @@ import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TE
 @Sql(executionPhase=AFTER_TEST_METHOD, scripts= {"/sql/database_cleanup.sql"})
 @NotThreadSafe
 public class NavBoxSearchTest {
-    public  final String mockServerUrl = "http://127.0.0.1";
+
     private String mockServerFullUrl;
 
     @Value("classpath:/json/elastic_search/search_response.json")
     private Resource searchResponseJson;
 
+    @Value("${nasnav.elasticsearch.url}")
+    private String mockServerUrl;
 
     @Rule
-    public MockServerRule mockServerRule = new MockServerRule(this);
+    public MockServerRule mockServerRule;
+
+
+    @Autowired
+    private TestRestTemplate template;
 
 
 
 
 
-    @Before
+    /**
+     * set the mockserver port before its initialized by @Rule
+     * */
+    @PostConstruct
     public  void initElasticSearchMockServer() throws Exception {
+        URI mockServerUri = new URI(mockServerUrl);
+        int port = mockServerUri.getPort();
+        mockServerRule = new MockServerRule(this, port);
+    }
+
+
+
+    /**
+     * Prepares the mocks server responses, this must run after both @PostConstruct
+     * and @Rule, as @Rule will create initialize the mock server.
+     * */
+    @Before
+    public void prepareMockServer() throws Exception {
         prepareMockRequests(mockServerRule);
-        mockServerFullUrl =  mockServerUrl + ":"+ mockServerRule.getPort();
+    }
+
+
+
+    @Test
+    public void searchTest(){
+        ResponseEntity<String> response =
+                template.getForEntity("/navbox/search?org_id=99001&keyword=givemebeefburger", String.class);
+        assertEquals(OK, response.getStatusCode());
     }
 
 
@@ -64,7 +105,8 @@ public class NavBoxSearchTest {
         mockServerRule.getClient()
                 .when(
                         request().withMethod("GET")
-                                .withPath("/_search"))
+                                .withPath("/_search")
+                                )
                 .respond(
                         response().withBody(productsResponse, JSON_UTF_8)
                                 .withStatusCode(200))
