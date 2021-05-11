627a9d09d7ae8777291d3fe092792317e065d801
Ahmad.Abdel-Salam <ahmad.abdel-salam@nasnav.com>
2021-05-11 13:44:19 +0200

    add filter by DELIVERED, FINALIZED only for sold products

--- nasnav-services/src/main/java/com/nasnav/dao/OrdersRepository.java
+++ nasnav-services/src/main/java/com/nasnav/dao/OrdersRepository.java
@@ -189,6 +189,7 @@ public interface OrdersRepository extends JpaRepository<OrdersEntity, Long> {
 			" left join stock.productVariantsEntity variant " +
 			" left join variant.productEntity product " +
 			" where product.organizationId = :orgId and subOrder.creationDate >= :startDate" +
+			" and subOrder.status in (5,8)" +
 			" GROUP BY product.id, product.name, variant.id, variant.name, DATE_TRUNC('month',subOrder.creationDate)" +
 			" order by DATE_TRUNC('month',subOrder.creationDate) desc, COUNT(product.id) desc")
 	List<ProductStatisticsInfo> getProductsStatisticsPerMonth(@Param("orgId") Long orgId,
