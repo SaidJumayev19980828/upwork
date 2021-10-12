01536b823a8cb469460366e26cbae77c0e6d0f14
Ahmad.Abdel-Salam <ahmad.abdel-salam@nasnav.com>
2021-10-12 12:41:37 +0200

    separate carts, wishlists, shipping apis into new controllers

---
+++ yeshtery/src/main/java/com/nasnav/yeshtery/controller/v1/YeshteryShippingController.java
@@ -0,0 +1,32 @@
+package com.nasnav.yeshtery.controller.v1;
+
+import com.nasnav.dto.request.shipping.ShippingOfferDTO;
+import com.nasnav.service.ShippingManagementService;
+import com.nasnav.yeshtery.YeshteryConstants;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping(YeshteryShippingController.API_PATH)
+@Tag(name = "Yeshtery Shipping Controller")
+@CrossOrigin("*")
+@EnableJpaRepositories
+public class YeshteryShippingController {
+
+    static final String API_PATH = YeshteryConstants.API_PATH +"/shipping";
+
+    @Autowired
+    private ShippingManagementService shippingService;
+
+    @GetMapping(path = "/offers", produces= APPLICATION_JSON_VALUE)
+    public List<ShippingOfferDTO> getShippingOffers(@RequestHeader(name = "User-Token", required = false) String token,
+                                                    @RequestParam("customer_address") Long customerAddress) {
+        return shippingService.getYeshteryShippingOffers(customerAddress);
+    }
+}
