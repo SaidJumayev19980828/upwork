c13819f664604844c7ef28eacc4e17c8c6f30f9f
Ahmad Abdel-Salam <ahmad.18750@yahoo.com>
2021-10-19 11:28:40 +0200

    return empty list instead of null in case of non existing images

--- nasnav-services/src/main/java/com/nasnav/dto/response/YeshteryOrganizationDTO.java
+++ nasnav-services/src/main/java/com/nasnav/dto/response/YeshteryOrganizationDTO.java
@@ -4,6 +4,7 @@ import com.nasnav.dto.OrganizationImagesRepresentationObject;
 import com.nasnav.dto.ShopRepresentationObject;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -11,6 +12,6 @@ public class YeshteryOrganizationDTO {
     private Long id;
     private String name;
     private String description;
-    List<OrganizationImagesRepresentationObject> images;
+    List<OrganizationImagesRepresentationObject> images = new ArrayList<>();
     List<ShopRepresentationObject> shops;
 }
