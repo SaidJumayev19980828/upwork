350527264143273d8f143f50c842c867920fe6b8
Bassam <b.lashen@hotmail.com>
2022-04-08 21:57:01 +0200

    revert

--- nasnav-services/src/main/resources/db/changelog/change-logs/00022-20220405-add_paymob_source_identifier.sql
+++ nasnav-services/src/main/resources/db/changelog/change-logs/00022-20220405-add_paymob_source_identifier.sql
@@ -8,8 +8,8 @@ ALTER TABLE public.paymob_source ADD identifier varchar;
 
 
 -- test data
-update public.paymob_source set identifier = 'Wallet', type ='+201110002373' where value = '1739267';
-update public.paymob_source set identifier = 'AGGREGATOR', type ='AGGREGATOR' where value = '1928804';
-update public.paymob_source set identifier = 'cash', type ='AGGREGATOR' where value = '1974407';
+    update public.paymob_source set identifier = 'Card', type ='Card' where value = '1739267';
+update public.paymob_source set identifier = '+201110002373', type ='WALLET' where value = '1928804';
+update public.paymob_source set identifier = 'cash', type ='Cash Collection' where value = '1974407';
 
 ALTER TABLE public.paymob_source ALTER COLUMN identifier SET NOT NULL;
