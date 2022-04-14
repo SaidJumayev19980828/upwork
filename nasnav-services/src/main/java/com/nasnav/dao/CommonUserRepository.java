d9113520914787660e7f0adf1d9dc0ac505e4914
ahmad.abdelsalam <ahmad.18750@yahoo.com>
2022-04-14 17:49:28 +0200

    open login api for employees
    (cherry picked from commit 85f36cc2b0259da4fffbc4ae12756d7c6aa13344)

--- nasnav-services/src/main/java/com/nasnav/dao/CommonUserRepository.java
+++ nasnav-services/src/main/java/com/nasnav/dao/CommonUserRepository.java
@@ -11,6 +11,6 @@ public interface CommonUserRepository{
 	List<String> getUserRoles(BaseUserEntity user);
 	BaseUserEntity saveAndFlush(BaseUserEntity userEntity) ;
 	BaseUserEntity getByEmailAndOrganizationId(String email, Long org_id);
-	BaseUserEntity getByEmailIgnoreCaseAndOrganizationId(String email, Long orgId, Boolean isEmployee, YeshteryState state);
+	BaseUserEntity getByEmailIgnoreCaseAndOrganizationId(String email, Long orgId, Boolean isEmployee);
 	Optional<BaseUserEntity> findById(Long id, Boolean isEmp);
 }
