34eebe124d6c7d9f49152afc6f9db76c39d2bb83
ahmad.abdelsalam <ahmad.18750@yahoo.com>
2022-06-26 17:09:59 +0200

    fix using dtos in callback response

--- nasnav-services/src/main/java/com/nasnav/payments/paymob/WebhookCallbackResponse.java
+++ nasnav-services/src/main/java/com/nasnav/payments/paymob/WebhookCallbackResponse.java
@@ -2,12 +2,20 @@ package com.nasnav.payments.paymob;
 
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class WebhookCallbackResponse {
     private RetrieveTransactionResponse obj;
     private String type;
-    @JsonProperty("transaction_processed_callback_responses")
     private String transactionProcessedCallbackResponses;
 }
