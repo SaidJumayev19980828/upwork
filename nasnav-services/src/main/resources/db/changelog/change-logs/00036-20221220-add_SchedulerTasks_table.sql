9de4ea0a7c53326590fc3cd4c4eff8ae40bfe40e
mohamed ismail <mohamed.ismail.ibrahim97@gmail.com>
2023-01-24 15:19:35 +0200

    fix liquibase

--- nasnav-services/src/main/resources/db/changelog/change-logs/00036-20221220-add_SchedulerTasks_table.sql
+++ nasnav-services/src/main/resources/db/changelog/change-logs/00036-20221220-add_SchedulerTasks_table.sql
@@ -9,8 +9,8 @@ create Table scheduler_tasks (
                                  id bigserial not null Primary Key,
                                  created_at timestamp without time zone NOT null default now(),
                                  starts_at timestamp without time zone NOT null default now(),
-                                 availability_id bigint null References Availabilities(id),
+                                 availability_id bigint null References availabilities(id),
                                  user_id bigint null References users(id),
                                  employee_user_id bigint null References employee_users(id),
-                                 type text null,
+                                 type text null
 );
