8dd317277cb333a0a061ce1554f1409bad457bbd
doaaabdelwahed <engdoaa.wahed@gmail.com>
2023-03-12 21:20:23 +0200

    update SQl file

--- nasnav-services/src/main/resources/db/changelog/change-logs/00037-20230307-add-addons.sql
+++ nasnav-services/src/main/resources/db/changelog/change-logs/00037-20230307-add-addons.sql
@@ -36,11 +36,11 @@ CREATE TABLE public.cart_item_addon_details(
     id  SERIAL PRIMARY KEY NOT NULL,
     addon_stock_id bigint,
 	cart_item_id bigint,
-    quantity integer
-    user_id bigint,
+    quantity integer,
+    user_id bigint
     
 );
---------------------------------------------
+
+-----------------------------------------------------------------------
 
 ALTER TABLE public.baskets ADD COLUMN  addon_price numeric(10,2) DEFAULT 0;
-
