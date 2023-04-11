0cfe11b46202d2919807ad6be1f7d60cd5462392
abdourakhmane <ripault.a@gmail.com>
2023-04-11 23:50:15 +0000

    [US1] gitlab-ci builds

--- deploy/docker_build.sh
+++ deploy/docker_build.sh
@@ -9,6 +9,7 @@ cd "$BASE_PATH"
 
 BRANCH=`echo $CI_COMMIT_REF_NAME | cut -d "-" -f 1`
 IMAGE_REGISTRY="$CI_REGISTRY_IMAGE"
+IMAGE_TAG_BRANCH=$IMAGE_REGISTRY:$BRANCH
 REVISION="$VERSION-$BRANCH+$CI_PIPELINE_ID.$(date --utc +%Y%m%dT%H%M%S)"
 IMAGE_TAG_FQN=$IMAGE_REGISTRY:$VERSION-$BRANCH
 
@@ -17,12 +18,14 @@ IMAGE_TAG_FQN=$IMAGE_REGISTRY:$VERSION-$BRANCH
 mkdir ./build
 echo "IMAGE_REGISTRY=$IMAGE_REGISTRY" > ./build/env
 echo "IMAGE_TAG_FQN=$IMAGE_TAG_FQN" >> ./build/env
+echo "IMAGE_TAG_BRANCH=$IMAGE_TAG_BRANCH" >> ./build/env
 echo "$REVISION" > ./build/REVISION
 
 #- build
 
 echo -e "${COLOR}-> Build image: $IMAGE_TAG_FQN${NC}"
 docker build -t $IMAGE_TAG_FQN .  -f ./deploy/Dockerfile.prod
+docker tag $IMAGE_TAG_FQN $IMAGE_TAG_BRANCH
 [ "$BRANCH" == "prod" ] && docker tag $IMAGE_TAG_FQN $IMAGE_REGISTRY:latest
 
 true
