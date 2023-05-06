eacaa7b7be48aafcb11fe922e87d9c13f2344805
Hussein Aassem <hussienasem@gmail.com>
2023-05-07 02:35:55 +0300

    RocketChatCustomerToken entity and repo

---
+++ nasnav-services/src/main/java/com/nasnav/dao/RocketChatCustomerTokenRepository.java
@@ -0,0 +1,11 @@
+package com.nasnav.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nasnav.persistence.RocketChatCustomerTokenEntity;
+import com.nasnav.persistence.UserEntity;
+import java.util.Optional;
+
+public interface RocketChatCustomerTokenRepository extends JpaRepository<RocketChatCustomerTokenEntity, Long> {
+	public Optional<RocketChatCustomerTokenEntity> findByUserEntity(UserEntity user);
+}
---
+++ nasnav-services/src/main/java/com/nasnav/persistence/RocketChatCustomerTokenEntity.java
@@ -0,0 +1,29 @@
+package com.nasnav.persistence;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity
+@Table(name = "rocket_chat_user_token")
+@EqualsAndHashCode(callSuper=false)
+public class RocketChatCustomerTokenEntity extends DefaultBusinessEntity<Integer> {
+	@OneToOne(optional = false)
+	@JoinColumn(name = "user_id", referencedColumnName = "id")
+	private UserEntity user;
+
+	@Column(name = "token")
+	@NotNull
+	private String token;
+
+	public RocketChatCustomerTokenEntity(String token) {
+		this.token = token;
+	}
+}
