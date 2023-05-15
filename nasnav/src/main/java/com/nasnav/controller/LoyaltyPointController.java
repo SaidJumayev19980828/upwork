2192d395cf102143ace33f79caff82f4a2a8037e
eslamsoftware93 <eslamsoftware93@gmail.com>
2023-05-15 10:31:46 +0300

    Add loyalty points to nasnav

---
+++ nasnav/src/main/java/com/nasnav/controller/LoyaltyPointController.java
@@ -0,0 +1,327 @@
+package com.nasnav.controller;
+
+import com.nasnav.commons.YeshteryConstants;
+import com.nasnav.dto.GiftDTO;
+import com.nasnav.dto.UserRepresentationObject;
+import com.nasnav.dto.request.*;
+import com.nasnav.dto.response.LoyaltyPointTransactionDTO;
+import com.nasnav.persistence.LoyaltyCoinsDropEntity;
+import com.nasnav.persistence.LoyaltyFamilyEntity;
+import com.nasnav.persistence.LoyaltyGiftEntity;
+import com.nasnav.persistence.UserEntity;
+import com.nasnav.persistence.dto.query.result.OrganizationPoints;
+import com.nasnav.response.*;
+import com.nasnav.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+import static com.nasnav.constatnts.DefaultValueStrings.INVALID_ID;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping(LoyaltyPointController.API_PATH)
+@CrossOrigin("*")
+public class LoyaltyPointController {
+    static final String API_PATH = YeshteryConstants.API_PATH +"/loyalty";
+
+    @Autowired
+    private LoyaltyPointsService loyaltyPointsService;
+    @Autowired
+    private LoyaltyFamilyService loyaltyFamilyService;
+    @Autowired
+    private LoyaltyTierService loyaltyTierService;
+    @Autowired
+    private LoyaltyCharityService loyaltyCharityService;
+    @Autowired
+    private LoyaltyGiftService loyaltyGiftService;
+    @Autowired
+    private LoyaltyCoinsDropService loyaltyCoinsDropService;
+    @Autowired
+    private LoyaltyBoosterService loyaltyBoosterService;
+    @Autowired
+    private LoyaltyEventService loyaltyEventService;
+
+
+    @GetMapping(value ="points")
+    public LoyaltyUserPointsResponse getUserPoints(@RequestHeader(name = "User-Token", required = false) String token,
+                                                   @RequestParam("org_id") Long orgId){
+        return loyaltyPointsService.getUserPoints(orgId);
+    }
+
+    @GetMapping(value ="spendable_points")
+    public List<LoyaltyPointTransactionDTO> getUserPoints(@RequestHeader(name = "User-Token", required = false) String token){
+        return loyaltyPointsService.getUserSpendablePoints();
+    }
+//    @PostMapping(value ="share_points")
+//    public void sharePoints(@RequestHeader(name = "User-Token", required = false) String token , @RequestParam("point_id")  Long pointId , @RequestParam("email")  String email ,@RequestParam("points") BigDecimal points){
+//         loyaltyPointsService.sharePoints(pointId ,email,points);
+//    }
+    @GetMapping(value ="spendable_points/{orgId}")
+    public List<LoyaltyPointTransactionDTO> getUserSpendablePoints(@RequestHeader(name = "User-Token", required = false) String token , @PathVariable long orgId){
+        return loyaltyPointsService.getUserSpendablePoints(orgId);
+    }
+
+    @GetMapping(value ="points_per_org")
+    public List<OrganizationPoints> getUserPointsPerOrg(@RequestHeader(name = "User-Token", required = false) String token){
+        return loyaltyPointsService.getUserPointsPerOrg();
+    }
+
+    @GetMapping(value = "points/list", produces = APPLICATION_JSON_VALUE)
+    public List<LoyaltyPointTransactionDTO> getLoyaltyPoints(@RequestHeader(name = "User-Token", required = false) String token, @RequestParam("org_id")  Long orgId ) {
+        return loyaltyPointsService.listOrganizationLoyaltyPoints(orgId);
+    }
+
+    @GetMapping(value ="user_tier")
+    public LoyaltyTierDTO getUserTier(@RequestHeader(name = "User-Token", required = false) String token,
+                                      @RequestParam("org_id") Long orgId){
+        return loyaltyPointsService.getUserOrgTier(orgId);
+    }
+
+    // CRUD Ops
+
+    /**
+     * Config APIs
+     */
+
+    @PostMapping(value = "config/update")
+    public LoyaltyPointsUpdateResponse updateLoyaltyPointConfig(@RequestHeader(name = "User-Token", required = false) String token,
+                                                                @RequestBody LoyaltyPointConfigDTO dto) {
+        return loyaltyPointsService.updateLoyaltyPointConfig(dto);
+    }
+
+    @DeleteMapping("config/delete")
+    public LoyaltyPointDeleteResponse deleteLoyaltyPointConfig(@RequestHeader(name = "User-Token", required = false) String token,
+                                                               @RequestParam Long id) {
+        return loyaltyPointsService.deleteLoyaltyPointConfig(id);
+    }
+
+    @GetMapping(value = "config/list", produces = APPLICATION_JSON_VALUE)
+    public List<LoyaltyPointConfigDTO> getLoyaltyPointConfigs(@RequestHeader(name = "User-Token", required = false) String token) {
+        return loyaltyPointsService.listLoyaltyPointConfigs();
+    }
+
+    @GetMapping(value = "config", produces = APPLICATION_JSON_VALUE)
+    public LoyaltyPointConfigDTO getLoyaltyPointConfig(@RequestHeader(name = "User-Token", required = false) String token) {
+        return loyaltyPointsService.getLoyaltyPointActiveConfig();
+    }
+
+
+    /**
+     * Tier APIs APIs
+     **/
+
+    @GetMapping(value = "tier")
+    public LoyaltyTierDTO getTierById(@RequestHeader(name = "User-Token", required = false) String token,
+                                      @RequestParam(value = "tier_id") Long tierId) {
+        return loyaltyTierService.getTierById(tierId);
+    }
+
+    @PostMapping(value = "tier/update")
+    public LoyaltyTierUpdateResponse createTier(@RequestHeader(name = "User-Token", required = false) String token,
+                                                @RequestBody LoyaltyTierDTO dto) {
+        return loyaltyTierService.updateTier(dto);
+    }
+
+    @GetMapping(value = "tier/list")
+    public List<LoyaltyTierDTO> getTier(@RequestHeader(name = "User-Token", required = false) String token,
+                                        @RequestParam(value = "is_special", required = false) Boolean isSpecial) {
+        return loyaltyTierService.getTiers(isSpecial);
+    }
+
+    @DeleteMapping(value = "tier/delete")
+    public void deleteTier(@RequestHeader(name = "User-Token", required = false) String token,
+                           @RequestParam(value = "tier_id") Long tierId) {
+        loyaltyTierService.deleteTier(tierId);
+    }
+
+    @PostMapping(value = "tier/change_user_tier")
+    public UserRepresentationObject changeUserTier(@RequestHeader(name = "User-Token", required = false) String token,
+                                                   @RequestParam(value = "tier_id") Long tierId,
+                                                   @RequestParam(value = "user_id") Long userId) {
+        return loyaltyTierService.changeUserTier(userId, tierId);
+    }
+
+
+    /*
+    @GetMapping(value = "points/check", produces = APPLICATION_JSON_VALUE)
+    public List<RedeemPointsOfferDTO> checkRedeemPoints(@RequestHeader(name = "User-Token", required = false) String token,
+                                                        @RequestParam String code) {
+        return loyaltyPointsService.checkRedeemPoints(code);
+    }
+    */
+    @PostMapping(value = "points/code/redeem")
+    public void redeemPoints(@RequestHeader(name = "User-Token", required = false) String token,
+                             @RequestParam("order_id") Long orderId,
+                             @RequestParam("code") String code) {
+        loyaltyPointsService.redeemPoints(orderId, code);
+    }
+
+
+    @GetMapping(value = "points/code/generate")
+    public String userScan(@RequestHeader(name = "User-Token", required = false) String token,
+                           @RequestParam("shop_id") Long shopId) {
+        return loyaltyPointsService.generateUserShopPinCode(shopId);
+    }
+
+
+    /***
+     * Family APIs
+     * **/
+
+    //@GetMapping(value = "family")
+    public Optional<LoyaltyFamilyEntity> getFamilyById(@RequestHeader(name = "User-Token", required = false) String token,
+                                                       @RequestParam(value = "family_id") Long familyId) {
+        return loyaltyFamilyService.getFamilyById(familyId);
+    }
+
+    //@PostMapping(value = "family/update")
+    public LoyaltyFamilyUpdateResponse createFamily(@RequestHeader(name = "User-Token", required = false) String token,
+                                                    @RequestBody LoyaltyFamilyDTO dto) {
+        return loyaltyFamilyService.updateFamily(dto);
+    }
+
+    //@GetMapping(value = "family/list")
+    public List<LoyaltyFamilyEntity> getFamily(@RequestHeader(name = "User-Token", required = false) String token,
+                                               @RequestParam(value = "org_id", required = false, defaultValue = INVALID_ID) Long orgId) {
+        return loyaltyFamilyService.listFamily(orgId);
+    }
+
+    //@PostMapping(value = "family/new_member")
+    public List<UserEntity> addNewMemberToFamily(@RequestHeader(name = "User-Token", required = false) String token,
+                                                 @RequestParam(value = "family_id") Long familyId,
+                                                 @RequestParam(value = "user_id") Long userId) {
+        
+        return loyaltyFamilyService.addNewMemberToFamily(userId, familyId);
+    }
+
+    //@DeleteMapping(value = "family/delete")
+    public void deleteFamily(@RequestHeader(name = "User-Token", required = false) String token,
+                             @RequestParam(value = "family_id") Long familyId) {
+        loyaltyFamilyService.deleteFamily(familyId);
+    }
+
+
+
+    /**
+     * Coins Drop APIs
+     *
+     */
+    //@PostMapping(value = "coins_drop/update")
+    public LoyaltyCoinUpdateResponse createCoin(@RequestHeader(name = "User-Token", required = false) String token,
+                                                @RequestBody LoyaltyCoinsDropDTO dto) {
+        return loyaltyCoinsDropService.updateCoinsDrop(dto);
+    }
+
+    //@GetMapping(value = "coins_drop/all")
+    public List<LoyaltyCoinsDropEntity> getCoinByOrgId(@RequestHeader(name = "User-Token", required = false) String token,
+                                                       @RequestParam(value = "org_id") Long orgId) {
+        return loyaltyCoinsDropService.getByOrganizationId(orgId);
+    }
+
+
+    /**
+     * Booster APIs
+     **/
+
+    //@GetMapping(value = "booster")
+    public LoyaltyBoosterDTO getBoosterById(@RequestHeader(name = "User-Token", required = false) String token,
+                                            @RequestParam(value = "booster_id") Long boosterId) {
+        return loyaltyBoosterService.getBoosterById(boosterId);
+    }
+
+    //@PostMapping(value = "booster/update")
+    public LoyaltyBoosterUpdateResponse createBooster(@RequestHeader(name = "User-Token", required = false) String token,
+                                                      @RequestBody LoyaltyBoosterDTO dto) {
+        return loyaltyBoosterService.updateBooster(dto);
+    }
+
+    //@GetMapping(value = "booster/list")
+    public List<LoyaltyBoosterDTO> getBooster(@RequestHeader(name = "User-Token", required = false) String token,
+                                              @RequestParam(value = "org_id", required = false, defaultValue = INVALID_ID) Long orgId) {
+        
+        return loyaltyBoosterService.getBoosters(orgId);
+    }
+
+    //@DeleteMapping(value = "booster/delete")
+    public void deleteBooster(@RequestHeader(name = "User-Token", required = false) String token,
+                              @RequestParam(value = "booster_id") Long boosterId) {
+        loyaltyBoosterService.deleteBooster(boosterId);
+    }
+
+    //@PostMapping(value = "booster/upgrade")
+    public void upgradeUserBooster(@RequestHeader(name = "User-Token", required = false) String token,
+                                   @RequestParam(value = "booster_id") Long boosterId,
+                                   @RequestParam(value = "user_id") Long userId) {
+        loyaltyBoosterService.upgradeUserBooster(boosterId, userId);
+    }
+
+    /**
+     * Charity APIs
+     *
+     */
+    //@PostMapping(value = "charity/update")
+    public LoyaltyCharityUpdateResponse createCharity(@RequestHeader(name = "User-Token", required = false) String token,
+                                                      @RequestBody LoyaltyCharityDTO dto) {
+        return loyaltyCharityService.updateCharity(dto);
+    }
+
+    //@PostMapping(value = "charity/user/update")
+    public LoyaltyCharityUpdateResponse createCharity(@RequestHeader(name = "User-Token", required = false) String token,
+                                                      @RequestBody UserCharityDTO dto) {
+        return loyaltyCharityService.updateUserCharity(dto);
+    }
+
+    //@PostMapping(value = "charity/user/donate")
+    public void donateUserToCharity(@RequestHeader(name = "User-Token", required = false) String token,
+                                    @RequestParam(value = "charity_id") Long charityId,
+                                    @RequestParam(value = "user_id") Long userId,
+                                    @RequestParam(value = "shop_id") Long shopId) {
+        loyaltyCharityService.updateOrCreateLoyaltyUserCharityTransaction(charityId, userId, shopId);
+    }
+
+    /**
+     * Gift APIs
+     *
+     */
+    //@PostMapping(value = "gift/send")
+    public LoyaltyGiftUpdateResponse createGift(@RequestHeader(name = "User-Token", required = false) String token,
+                                                @RequestBody GiftDTO dto) {
+        return loyaltyGiftService.sendGiftFromUserToAnother(dto);
+    }
+
+    //@GetMapping(value = "gift/all")
+    public List<LoyaltyGiftEntity> getUserGifts(@RequestHeader(name = "User-Token", required = false) String token,
+                                                @RequestParam(value = "user_id") Long userId) {
+        return loyaltyGiftService.getGiftsByUserId(userId);
+    }
+
+    //@GetMapping(value = "gift/redeem")
+    public List<LoyaltyGiftEntity> getUserGiftsNotRedeem(@RequestHeader(name = "User-Token", required = false) String token,
+                                                         @RequestParam(value = "user_id") Long userId,
+                                                         @RequestParam(value = "is_redeem", required = false) Boolean isRedeem) {
+        return loyaltyGiftService.getGiftsByUserIdAndIsRedeem(userId, isRedeem);
+    }
+
+    /**
+     * Events API
+     */
+
+    //@GetMapping("event")
+    public List<LoyaltyEventDTO> getAllEvents(@RequestHeader(name = "User-Token", required = false) String token, @RequestParam("org_id") Long orgId){
+        return loyaltyEventService.getAllEvents(orgId);
+    }
+    //@PostMapping("event")
+    public  LoyaltyEventUpdateResponse createUpdateEvent(@RequestHeader(name = "User-Token", required = false) String token, @RequestBody LoyaltyEventDTO request){
+        return loyaltyEventService.createUpdateEvent(request);
+    }
+
+    //@DeleteMapping("event/{id}")
+    public void deleteEvent(@RequestHeader(name = "User-Token", required = false) String token, @PathVariable Long id){
+        loyaltyEventService.deleteById(id);
+    }
+
+}
---
+++ nasnav/src/test/resources/sql/database_cleanup_loyalty_points.sql
@@ -0,0 +1,128 @@
+
+DELETE FROM public.integration_mapping where organization_id BETWEEN 99000 AND 99999;
+DELETE FROM public.integration_param where  organization_id BETWEEN 99000 AND 99999;
+DELETE FROM public.integration_event_failure where organization_id BETWEEN 99000 AND 99999;
+DELETE FROM public.integration_mapping_type;
+DELETE FROM public.integration_param_type;
+delete from public.event_requests;
+delete from public.event_products;
+delete from public.event_attachments;
+delete from public.event_logs;
+DELETE FROM public.EVENTS;
+delete from public.influencer_categories;
+delete from public.influencers;
+DELETE FROM public.api_logs WHERE organization_id between 99000 AND 99999;
+DELETE FROM public.products_extra_attributes WHERE extra_attribute_id IN (SELECT id FROM public.extra_attributes WHERE organization_id BETWEEN 99000 AND 99999);
+DELETE FROM public.extra_attributes WHERE organization_id BETWEEN 99000 AND 99999;
+DELETE FROM public.role_employee_users WHERE employee_user_id IN (SELECT id FROM public.employee_users WHERE organization_id BETWEEN 99000 AND 99999);
+DELETE FROM public.product_bundles WHERE product_id IN (SELECT id FROM public.products WHERE organization_id BETWEEN 99000 AND 99999);
+DELETE FROM public.shipment where id IN (
+    SELECT shp.id FROM public.shipment shp
+                           LEFT JOIN public.orders ord on shp.sub_order_id = ord.id
+    WHERE ord.organization_id BETWEEN 99000 AND 99999);
+DELETE FROM public.organization_shipping_service WHERE organization_id BETWEEN 99000 AND 99999;
+DELETE FROM public.shipping_areas;
+DELETE FROM public.shipping_service;
+DELETE FROM meta_orders_promotions;
+DELETE FROM public.PROMOTIONS_CART_CODES;
+DELETE FROM public.PROMOTIONS_CODES_USED;
+DELETE FROM public.promotions;
+DELETE FROM public.cart_items where id in (
+    select crt.id
+    from public.cart_items crt
+             left join public.users usr on crt.user_id  = usr.id
+    where usr.organization_id between 99000 and 99999
+);
+DELETE FROM public.return_request_item WHERE return_request_id IN
+                                             (SELECT Id from public.return_request where meta_order_id in
+                                                                                         (select id from public.meta_orders where organization_id between 99000 and 99999));
+DELETE FROM public.return_request where meta_order_id in
+                                        (select id from public.meta_orders where organization_id between 99000 and 99999);
+DELETE FROM public.return_shipment where id in (
+    select return_shipment_id
+    from public.return_request_item it
+             left join public.return_request req
+                       on it.return_request_id = req.id
+             inner join public.meta_orders meta
+                        on req.meta_order_id = meta.id
+                            and meta.organization_id between 99000 and 99999
+);
+DELETE FROM public.return_shipment where shipping_service_id = 'TEST';
+DELETE FROM public.loyalty_spent_transactions WHERE transaction_id in (select id from loyalty_point_transactions WHERE org_id between 99000 and 99999);
+DELETE FROM public.loyalty_point_transactions WHERE org_id between 99000 and 99999;
+DELETE FROM public.baskets WHERE stock_id IN (SELECT Id from public.stocks where organization_id between 99000 and 99999);
+DELETE FROM public.orders WHERE organization_id BETWEEN 99000 AND 99999;
+DELETE FROM public.payments WHERE user_id IN (SELECT Id from public.users where organization_id between 99000 and 99999);
+DELETE FROM public.meta_orders where organization_id between 99000 and 99999;
+DELETE FROM public.stocks WHERE organization_id BETWEEN 99000 AND 99999;
+DELETE FROM public.units;
+DELETE FROM public.product_tags WHERE product_id IN (SELECT id from public.products where organization_id between 99000 and 99999);
+DELETE FROM public.variant_feature_values WHERE feature_id BETWEEN 234 AND 240;
+DELETE FROM public.product_features where organization_id between 99000 and 99999;
+DELETE FROM public.scheduler_tasks;
+DELETE FROM public.availabilities;
+DELETE FROM public.product_images WHERE
+        product_id IN (SELECT id from public.products where organization_id between 99000 and 99999)
+                                     or variant_id in (SELECT id FROM public.product_variants WHERE product_id IN (SELECT id FROM public.products WHERE organization_id BETWEEN 99000 AND 99999));
+DELETE FROM public.product_collections WHERE product_id IN (SELECT id FROM public.products WHERE organization_id BETWEEN 99000 AND 99999);
+DELETE FROM public.product_ratings where variant_id in
+                                         (SELECT id FROM public.product_variants WHERE product_id IN (SELECT id FROM public.products WHERE organization_id BETWEEN 99000 AND 99999));
+DELETE FROM public.product_variants WHERE product_id IN (SELECT id FROM public.products WHERE organization_id BETWEEN 99000 AND 99999);
+DELETE FROM public.shop360_products where shop_id in (select id from public.shops WHERE organization_id BETWEEN 99000 AND 99999);
+DELETE FROM public.products WHERE organization_id BETWEEN 99000 AND 99999;
+DELETE FROM public.roles WHERE organization_id BETWEEN 99000 AND 99999;
+DELETE FROM public.user_addresses WHERE user_id in (select id from public.users WHERE organization_id BETWEEN 99000 AND 99999);
+DELETE FROM public.user_tokens WHERE user_id in (select id from users WHERE organization_id BETWEEN 99000 AND 99999)
+                                  or employee_user_id in (select id from employee_users WHERE organization_id BETWEEN 99000 AND 99999);
+DELETE FROM public.loyalty_gift WHERE user_from_id in (select id from users WHERE organization_id BETWEEN 99000 AND 99999);
+DELETE FROM public.user_charity WHERE user_id in (select id from users WHERE organization_id BETWEEN 99000 AND 99999);
+DELETE FROM public.yeshtery_user_otp WHERE user_id in (SELECT id FROM public.yeshtery_users WHERE organization_id BETWEEN 99000 AND 99999);
+DELETE FROM public.user_otp WHERE user_id in (SELECT id FROM public.users WHERE organization_id BETWEEN 99000 AND 99999);
+DELETE FROM public.yeshtery_user_tokens WHERE yeshtery_user_id in (SELECT id FROM public.yeshtery_users WHERE organization_id BETWEEN 99000 AND 99999);
+DELETE FROM public.yeshtery_users WHERE organization_id BETWEEN 99000 AND 99999;
+DELETE FROM public.users WHERE organization_id BETWEEN 99000 AND 99999;
+DELETE FROM public.employee_users WHERE organization_id BETWEEN 99000 AND 99999;
+DELETE FROM public.social_links WHERE organization_id BETWEEN 99000 AND 99999;
+DELETE FROM public.organization_images where organization_id between 99000 and 99999;
+DELETE FROM public.files  where organization_id between 99000 and 99999;
+DELETE FROM public.files  where orig_filename = 'nasnav--Test_Photo.png';  -- for test files with no organization
+DELETE FROM public.scenes WHERE organization_id between 99000 and 99999;
+DELETE FROM public.shop_sections WHERE organization_id between 99000 and 99999;
+DELETE FROM public.shop_floors WHERE organization_id between 99000 and 99999;
+DELETE FROM public.shop360s WHERE shop_id in (select id from public.shops WHERE organization_id BETWEEN 99000 AND 99999);
+DELETE FROM public.loyalty_point_config WHERE  organization_id between 99000 and 99999;
+DELETE FROM public.loyalty_pins WHERE shop_id in (select id from shops where organization_id BETWEEN 99000 AND 99999);
+DELETE FROM public.shops WHERE organization_id BETWEEN 99000 AND 99999;
+DELETE FROM public.addresses where (id between 12300001 and 12300100) or address_line_1 = '630f3256-59bb-4b87-9600-60e64d028d68';
+DELETE FROM public.brands WHERE organization_id BETWEEN 99000 AND 99999;
+DELETE FROM public.tag_graph_edges
+WHERE child_id IN (
+    select node.id
+    from public.tag_graph_nodes node
+             left join tags tag on node.tag_id = tag.id
+    where tag.organization_id BETWEEN 99000 AND 99999);
+DELETE FROM public.tag_graph_nodes where tag_id  in (select id FROM public.tags WHERE organization_id BETWEEN 99000 AND 99999);
+DELETE FROM public.organization_themes WHERE organization_id BETWEEN 99000 AND 99999;
+DELETE FROM public.tags WHERE organization_id BETWEEN 99000 AND 99999;
+DELETE FROM public.organization_themes_settings where organization_id BETWEEN 99000 AND 99999;
+DELETE FROM public.organization_theme_classes where organization_id BETWEEN 99000 AND 99999;
+DELETE FROM public.organization_domains WHERE organization_id BETWEEN 99000 AND 99999;
+DELETE FROM public.settings  WHERE organization_id BETWEEN 99000 AND 99999;
+DELETE FROM public.organiztion_cart_optimization  WHERE organization_id BETWEEN 99000 AND 99999;
+DELETE FROM public.user_subscriptions where organization_id BETWEEN 99000 AND 99999;
+DELETE FROM public.organization_payments where organization_id BETWEEN 99000 AND 99999;
+DELETE FROM public.seo_keywords where organization_id BETWEEN 99000 AND 99999;
+DELETE FROM public.sub_areas where organization_id BETWEEN 99000 AND 99999;
+DELETE FROM public.loyalty_booster  where organization_id BETWEEN 99000 AND 99999;
+DELETE FROM public.loyalty_points  where organization_id BETWEEN 99000 AND 99999;
+DELETE FROM public.loyalty_tier  where organization_id BETWEEN 99000 AND 99999;
+DELETE FROM public.organizations WHERE id BETWEEN 99000 AND 99999;
+DELETE FROM public.shipping_areas;
+DELETE FROM public.areas;
+DELETE FROM public.cities;
+DELETE FROM public.countries;
+DELETE FROM public.organization_image_types;
+DELETE FROM public.themes where id between 5001 and 5003;
+DELETE FROM public.theme_classes where id between 990011 and 990012;
+DELETE FROM public.categories WHERE id between 200 AND 240;
+delete from public.loyalty_point_types where id between  31001 and 31999;
