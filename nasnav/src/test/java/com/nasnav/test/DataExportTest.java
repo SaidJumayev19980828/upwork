6d411e22703b92c1a09bf2bb990444ea701fdc4f
ahmedeid980 <ahmedzidan7575@gmail.com>
2023-08-10 14:45:29 +0300

    BUG307 | add database cleanup sql to method testProductExportXLSXForWholeOrganization

--- nasnav/src/test/java/com/nasnav/test/DataExportTest.java
+++ nasnav/src/test/java/com/nasnav/test/DataExportTest.java
@@ -186,6 +186,7 @@ public class DataExportTest extends AbstractTestWithTempBaseDir {
 
 	@Test
 	@Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD , scripts = {"/sql/Products_Export_Test_Data.sql", "/sql/Products_Export_Test_Extra_Data.sql", "/sql/database_cleanup.sql"})
+	@Sql(executionPhase = ExecutionPhase.AFTER_TEST_METHOD , scripts = {"/sql/database_cleanup.sql"})
 	public void testProductExportXLSXForWholeOrganization(){
 		var response =
 				template.exchange("/export/products/xlsx", GET, getHttpEntity("192021"), String.class);
