96a2e931631669a687f2ef571c9711cfb8613fcc
MoamenZakariaMohamed <moamenzakaria99@gmail.com>
2023-09-24 15:10:43 +0300

    change auth roles for influencer/response end-point

--- yeshtery/src/main/java/com/nasnav/yeshtery/security/SecurityConfiguration.java
+++ yeshtery/src/main/java/com/nasnav/yeshtery/security/SecurityConfiguration.java
@@ -189,7 +189,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             patternOf( "/v1/event"                   , DELETE    , setOf(ORGANIZATION_ADMIN, ORGANIZATION_MANAGER)),
             patternOf( "/v1/event/list/**"                                , getAllRoles()),
             patternOf( "/v1/influencer/host/**"             , getAllRoles()),
-            patternOf( "/v1/influencer/response"             ,POST                 , setOf(NASNAV_ADMIN)),
+            //TODO change roles so that the testing process continues the old one is setOf(NASNAV_ADMIN)
+            patternOf( "/v1/influencer/response"             ,POST                 ,  setOf(ORGANIZATION_ADMIN, ORGANIZATION_MANAGER)),
             patternOf( "/v1/influencer/hostingRequests"             ,GET                 , getNonCustomersRoles()),
             patternOf("/v1/loyalty/points/update"						, setOf(ORGANIZATION_ADMIN, ORGANIZATION_MANAGER)),
             patternOf("/v1/loyalty/points"					, GET	, setOf(CUSTOMER)),
