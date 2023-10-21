4f8133dd33dca1ad631676d019110ac53ea55f46
Hussein Aassem <hussienasem@gmail.com>
2023-10-21 23:49:26 +0300

    swagger schema

--- nasnav/src/main/java/com/nasnav/controller/ChatController.java
+++ nasnav/src/main/java/com/nasnav/controller/ChatController.java
@@ -8,13 +8,16 @@ import com.nasnav.dto.rocketchat.RocketChatVisitorDTO;
 import com.nasnav.service.rocketchat.AgentRocketChatService;
 import com.nasnav.service.rocketchat.CustomerRocketChatService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import static com.nasnav.constatnts.EntityConstants.TOKEN_HEADER;
-
 
 @RestController
 @RequestMapping("/chat")
@@ -23,11 +26,17 @@ public class ChatController {
 	private final CustomerRocketChatService customerRocketChatService;
 	private final AgentRocketChatService agentRocketChatService;
 
+	@Operation(responses = {
+			@ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = RocketChatVisitorDTO.class)))
+	})
 	@PostMapping("visitor")
 	public Mono<RocketChatVisitorDTO> getInitialVisitorData(@RequestHeader(TOKEN_HEADER) String userToken) {
 		return customerRocketChatService.getRocketChatVisitorData();
 	}
 
+	@Operation(responses = {
+			@ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = RocketChatAgentTokenDTO.class)))
+	})
 	@PostMapping("agent/authenticate")
 	public Mono<RocketChatAgentTokenDTO> createAgentAuthToken(@RequestHeader(TOKEN_HEADER) String userToken) {
 		return agentRocketChatService.createAgentTokenForCurrentEmployeeCreateAgentIfNeeded();
