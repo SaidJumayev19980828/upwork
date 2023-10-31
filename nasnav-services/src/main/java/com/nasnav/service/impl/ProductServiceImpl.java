a94676d709d791441766e25d52e37913b9d9f1d0
mohamed alaa <moalaa9528@gmail.com>
2023-10-31 02:19:13 +0200

    DEVOPS196 update product response to avoid nulls.

--- nasnav-services/src/main/java/com/nasnav/service/impl/ProductServiceImpl.java
+++ nasnav-services/src/main/java/com/nasnav/service/impl/ProductServiceImpl.java
@@ -1254,7 +1254,7 @@ private Supplier<List<PromotionsEntity>> getPromosSupplier(ProductSearchParam pa
 					.map(s -> setProductShops(s, product360Shops))
 					.map(s -> setProductRating(s, productRatings))
 					.collect(toList());
-			if (searchParam.has_promotions || !searchParam.promo_id.isEmpty()) {
+			if (searchParam.has_promotions || searchParam.promo_id != null) {
 				Set<Long> productIds = stocks.stream().map(ProductRepresentationObject::getId).collect(toSet());
 				ItemsPromotionsDTO promotionsDTO = promotionsService.getPromotionsListFromProductsAndBrandsAndTagsLists(productIds, emptySet(), emptySet(), searchParam.promotions_per_item);
 				setProductsPromotions(promotionsDTO, stocks);
