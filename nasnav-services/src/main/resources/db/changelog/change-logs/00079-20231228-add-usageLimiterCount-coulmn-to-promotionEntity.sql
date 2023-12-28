8917d36aa5769f385dfc779f2bfc127cc9fae45e
OmarMoaaz <omar.moaz@sbs-sa.net>
2023-12-28 23:01:10 +0200

    change dataTYpe

--- nasnav-services/src/main/resources/db/changelog/change-logs/00079-20231228-add-usageLimiterCount-coulmn-to-promotionEntity.sql
+++ nasnav-services/src/main/resources/db/changelog/change-logs/00079-20231228-add-usageLimiterCount-coulmn-to-promotionEntity.sql
@@ -5,4 +5,4 @@
 --comment: add usageLimiterCount to promotion table to limit the usage of the promoCode
 
 ALTER TABLE promotions
-    ADD COLUMN IF NOT EXISTS usage_limiter_count BIGINT NULL;
+    ADD COLUMN IF NOT EXISTS usage_limiter_count INTEGER NULL;
