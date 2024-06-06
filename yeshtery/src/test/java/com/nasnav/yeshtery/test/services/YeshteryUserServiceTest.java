493b05555148baa58c4b57c2f7d0a17c04dfeed3
sah2020 <akmaljonsamandarov@gmail.com>
2024-06-06 16:29:15 +0500

    Add unit tests for yeshtery user service

--- yeshtery/src/test/java/com/nasnav/yeshtery/test/services/YeshteryUserServiceTest.java
+++ yeshtery/src/test/java/com/nasnav/yeshtery/test/services/YeshteryUserServiceTest.java
@@ -1,20 +1,30 @@
 package com.nasnav.yeshtery.test.services;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+import static org.springframework.http.HttpStatus.NOT_ACCEPTABLE;
 
-import java.util.Optional;
-import java.util.stream.Stream;
+import java.time.LocalDateTime;
+import java.util.*;
 
+import com.nasnav.dao.CommonUserRepository;
+import com.nasnav.dao.UserAddressRepository;
+import com.nasnav.dao.yeshtery.CommonYeshteryUserRepository;
+import com.nasnav.dto.UserRepresentationObject;
+import com.nasnav.enumerations.Roles;
+import com.nasnav.exceptions.BusinessException;
+import com.nasnav.exceptions.RuntimeBusinessException;
+import com.nasnav.persistence.UserAddressEntity;
+import com.nasnav.service.RoleService;
+import com.nasnav.service.SecurityService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.NullSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.nasnav.dao.OrganizationRepository;
@@ -31,36 +41,155 @@ class YeshteryUserServiceTest {
 	@Mock
 	private OrganizationRepository organizationRepository;
 
+	@Mock
+	private SecurityService securityService;
+
+	@Mock
+	private RoleService roleService;
+
+	@Mock
+	private CommonUserRepository commonNasnavUserRepo;
+
+	@Mock
+	private CommonYeshteryUserRepository commonUserRepo;
+
+	@Mock
+	private UserAddressRepository userAddressRepo;
+
+	private UserEntity currentUser;
+	private UserRepresentationObject currentUserRep;
+
+
 	@InjectMocks
 	private YeshteryUserService yeshteryUserService = new YeshteryUserServiceImpl();
 
-	private static Stream<Arguments> generator() {
-		UserEntity user = new UserEntity();
-		user.setOrganizationId(99002L);
-		user.setYeshteryUserId(5L);
+	@BeforeEach
+	public void setUp() {
+		currentUser = new UserEntity();
+		currentUser.setId(2L);
+		currentUser.setOrganizationId(99002L);
 
-		return Stream.of(
-				Arguments.of(user));
+		currentUserRep = new UserRepresentationObject();
+		currentUserRep.setId(2L);
 	}
 
-	@ParameterizedTest
-	@MethodSource("generator")
-	@NullSource
-	void testGetUserForOrg(UserEntity repoUser) {
+	@Test
+	void testGetUserForOrg() {
 		UserEntity user = new UserEntity();
 		user.setOrganizationId(99001L);
 		user.setYeshteryUserId(5L);
 
-		Mockito.when(nasnavUserRepository.findByYeshteryUserIdAndOrganizationId(user.getYeshteryUserId(),
+		when(nasnavUserRepository.findByYeshteryUserIdAndOrganizationId(user.getYeshteryUserId(),
 				99002L))
-				.thenReturn(Optional.ofNullable(repoUser));
+				.thenReturn(Optional.ofNullable(currentUser));
 
-		Mockito.when(organizationRepository.existsByIdAndYeshteryState(99002L, 1)).thenReturn(true);
-		Mockito.when(organizationRepository.existsByIdAndYeshteryState(99001L, 1)).thenReturn(true);
+		when(organizationRepository.existsByIdAndYeshteryState(99002L, 1)).thenReturn(true);
+		when(organizationRepository.existsByIdAndYeshteryState(99001L, 1)).thenReturn(true);
 
 		UserEntity returnedUser = yeshteryUserService.getUserForOrg(user, 99002L);
-		assertEquals(repoUser, returnedUser);
+		assertEquals(currentUser, returnedUser);
 
-		Mockito.verifyNoMoreInteractions(nasnavUserRepository, organizationRepository);
+		verifyNoMoreInteractions(nasnavUserRepository, organizationRepository);
+	}
+
+	@Test
+	public void testGetYeshteryUserData_CurrentUserIsCustomer() throws BusinessException {
+		when(securityService.getCurrentUser()).thenReturn(currentUser);
+		when(securityService.currentUserIsCustomer()).thenReturn(true);
+		mockUserRepresentation(currentUser);
+
+		UserRepresentationObject result = yeshteryUserService.getYeshteryUserData(2L, true);
+
+		assertNotNull(result);
+		verify(securityService).getCurrentUser();
+		verify(securityService).currentUserIsCustomer();
+		verifyNoMoreInteractions(securityService, roleService, commonNasnavUserRepo, commonUserRepo, userAddressRepo);
+	}
+
+	@Test
+	public void testGetYeshteryUserData_UserIdIsNull() throws BusinessException {
+		when(securityService.getCurrentUser()).thenReturn(currentUser);
+		mockUserRepresentation(currentUser);
+
+		UserRepresentationObject result = yeshteryUserService.getYeshteryUserData(null, true);
+
+		assertNotNull(result);
+		verify(securityService).getCurrentUser();
+	}
+
+	@Test
+	public void testGetYeshteryUserData_CurrentUserNotAllowedToViewNonEmployees() {
+		when(securityService.getCurrentUser()).thenReturn(currentUser);
+		when(securityService.currentUserIsCustomer()).thenReturn(false);
+		when(roleService.getEmployeeHighestRole(currentUser.getId())).thenReturn(Roles.ORGANIZATION_EMPLOYEE);
+
+		RuntimeBusinessException exception = assertThrows(RuntimeBusinessException.class, () -> {
+			yeshteryUserService.getYeshteryUserData(2L, false);
+		});
+
+		Assertions.assertEquals(NOT_ACCEPTABLE, exception.getHttpStatus());
+		verify(securityService).getCurrentUser();
+		verify(securityService).currentUserIsCustomer();
+		verify(roleService).getEmployeeHighestRole(currentUser.getId());
+		verifyNoMoreInteractions(securityService, roleService, commonNasnavUserRepo, commonUserRepo, userAddressRepo);
+	}
+
+	@Test
+	public void testGetYeshteryUserData_UserNotFound() {
+		when(securityService.getCurrentUser()).thenReturn(currentUser);
+		when(securityService.currentUserIsCustomer()).thenReturn(false);
+		when(roleService.getEmployeeHighestRole(currentUser.getId())).thenReturn(Roles.ORGANIZATION_ADMIN);
+		when(commonNasnavUserRepo.findById(2L, true)).thenReturn(Optional.empty());
+
+		RuntimeBusinessException exception = assertThrows(RuntimeBusinessException.class, () -> {
+			yeshteryUserService.getYeshteryUserData(2L, true);
+		});
+
+		Assertions.assertEquals(NOT_ACCEPTABLE, exception.getHttpStatus());
+		verify(securityService).getCurrentUser();
+		verify(securityService).currentUserIsCustomer();
+		verify(roleService).getEmployeeHighestRole(currentUser.getId());
+		verify(commonNasnavUserRepo).findById(2L, true);
+		verifyNoMoreInteractions(securityService, roleService, commonNasnavUserRepo, commonUserRepo, userAddressRepo);
+	}
+
+	@Test
+	public void testGetYeshteryUserData_Success() throws BusinessException {
+		UserEntity user = new UserEntity();
+		user.setId(2L);
+		UserRepresentationObject userRep = new UserRepresentationObject();
+		userRep.setId(2L);
+
+		when(securityService.getCurrentUser()).thenReturn(currentUser);
+		when(securityService.currentUserIsCustomer()).thenReturn(false);
+		when(roleService.getEmployeeHighestRole(currentUser.getId())).thenReturn(Roles.ORGANIZATION_ADMIN);
+		when(commonNasnavUserRepo.findById(2L, true)).thenReturn(Optional.of(user));
+		mockUserRepresentation(user);
+
+		UserRepresentationObject result = yeshteryUserService.getYeshteryUserData(2L, true);
+
+		Assertions.assertNotNull(result);
+		verify(securityService).getCurrentUser();
+		verify(securityService).currentUserIsCustomer();
+		verify(roleService).getEmployeeHighestRole(currentUser.getId());
+		verify(commonNasnavUserRepo).findById(2L, true);
+		verify(commonUserRepo).getUserRoles(user);
+		verify(userAddressRepo).findByUser_Id(userRep.getId());
+	}
+
+	private void mockUserRepresentation(UserEntity user) {
+		UserRepresentationObject userRep = user.getRepresentation();
+		List<UserAddressEntity> addresses = new ArrayList<>();
+		List<String> roles = List.of(Roles.NASNAV_ADMIN.name());
+
+		when(userAddressRepo.findByUser_Id(userRep.getId())).thenReturn(addresses);
+		when(commonUserRepo.getUserRoles(user)).thenReturn(roles);
+		when(securityService.getLastLoginForUser(user)).thenReturn(LocalDateTime.now());
+
+		userRep.setAddresses(new ArrayList<>());
+		userRep.setRoles(Set.of(Roles.NASNAV_ADMIN.name()));
+		userRep.setIsInfluencer(false);
+		userRep.setLastLogin(LocalDateTime.now());
+		userRep.setDateOfBirth(LocalDateTime.now());
 	}
 }
